381a5ce4e393c25dfa5224fe1ff7265044ca164a
vimal Raveendran <vimalraveendra@gmail.com>
2020-10-27 22:12:25 +0100

    Updated script file to render meals based on the search input text

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let favList= document.getElementById('list');
 let mealContainer = document.getElementById('meal-container')
+let input = document.getElementById('input-text');
+let searchBtn = document.getElementById('search')
 
  const  getMeals=async()=>{
-   
     let  response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken`)
     let favMeals = await response.json();
     renderMeals(favMeals)
  }
+
+// when webpage initializes invoking the meals with category chicken
  getMeals()
 
 const  getFavMeals=async(e)=>{
@@ -27,8 +30,8 @@ const addMeals = (meals)=>{
         <img src="${meals.strMealThumb}" alt="${meals.strMeal}">
             <div class="meal-body">
                 <h4>${meals.strMeal.length < 20 ? `${meals.strMeal}` : `${meals.strMeal.substring(0, 20)}...`}</h4>
-                <button>
-                   <i class="fas fa-heart"></i>
+                <button id="fav">
+                   <i class="far fa-heart"></i>
                 </button>
             </div>
     </div>
@@ -45,4 +48,18 @@ const renderMeals = (mealList)=>{
   
 }
 
+const searchMeals= async()=>{
+    let searchInput = input.value
+    let  response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`)
+    let favMeals = await response.json();
+    if(favMeals.meals){
+        mealContainer.innerHTML="";
+        renderMeals(favMeals )
+        
+    }
+    searchInput ="";
+    
+}
+
 favList .addEventListener('click',getFavMeals)
+searchBtn.addEventListener('click',searchMeals)
